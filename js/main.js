7c01008570d016c77a058418fee3e91ff1389a43
Bao Duy <tranbaoduy20@gmail.com>
2022-12-06 21:14:30 +0700

    Finished

--- js/main.js
+++ js/main.js
@@ -1,10 +1,27 @@
 const container = document.querySelector('.container')
 const row = document.querySelector('.row')
 const buttonClean = document.querySelector('.clean-up')
+const buttonErase = document.querySelector('.erase')
+const buttonRainBow = document.querySelector('.rain-bow')
+const buttonNormal = document.querySelector('.normal')
 const inputRange = document.querySelector('.input-range')
+const range = document.querySelector('label')
+
+range.textContent = 16
 
 const size = 600
-let clear = 0
+const colorRange = [
+  'black',
+  'white',
+  'orange',
+  'red',
+  'blue',
+  'purple',
+  'yellow',
+  'limegreen',
+  'pink',
+]
+let color = 0
 
 function getSize(square) {
   return size / square
@@ -33,9 +50,10 @@ function makeSquare(square = 16) {
   container.style.gridTemplateRows = `repeat(${square},1fr)`
 }
 
-function draw(e) {
-  if (clear) div.style.backgroundColor = 'white'
-  else this.style.backgroundColor = 'black'
+function draw() {
+  if (color === 0) this.style.backgroundColor = `${colorRange[color]}`
+  if (color === 1) this.style.backgroundColor = `${colorRange[color]}`
+  if (color === -1) this.style.backgroundColor = `${colorRange[randomColor()]}`
 }
 
 function cleanUp() {
@@ -45,14 +63,43 @@ function cleanUp() {
   })
 }
 
+function randomColor() {
+  let random = Math.floor(Math.random() * colorRange.length) + 2
+  return random
+}
+
+function erase() {
+  color = 1
+}
+
+function rainBow() {
+  color = -1
+}
+
+function normal() {
+  color = 0
+}
+
 buttonClean.addEventListener('click', (e) => {
   cleanUp()
 })
 
+buttonErase.addEventListener('click', (e) => {
+  erase()
+})
+
+buttonRainBow.addEventListener('click', (e) => {
+  rainBow()
+})
+
+buttonNormal.addEventListener('click', (e) => {
+  normal()
+})
+
 inputRange.addEventListener('change', () => {
+  range.textContent = inputRange.value
   reSize()
-  console.log(inputRange.value)
   makeSquare(inputRange.value)
 })
 
-makeSquare(51)
+makeSquare()
